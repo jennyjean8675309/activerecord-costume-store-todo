9e5ec5d48ef4d588d91e47ae20d2f0db1d87beca
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-09-26 10:48:20 -0400

    Completed Costume Store todo lab using ActiveRecord

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,13 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+    create_table :haunted_houses do |t|
+        t.string :name
+        t.string :location
+        t.string :theme
+        t.integer :price
+        t.boolean :family_friendly
+        t.string :opening_date
+        t.string :closing_date
+        t.text :long_description
+    end
+end
---
+++ db/migrate/006_rename_in_business_on_costume_stores.rb
@@ -0,0 +1,5 @@
+class RenameInBusinessOnCostumeStores < ActiveRecord::Migration[5.2]
+    def change
+        rename_column :costume_stores, :in_business, :still_in_business
+    end
+end
